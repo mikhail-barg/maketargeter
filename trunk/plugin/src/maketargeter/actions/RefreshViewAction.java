ad1d359d8313d5ec19734d019741349659fa925b
mike_mvpi <mike_mvpi@9010630c-1fa5-4ed3-80d8-4f5a8435ba9c>
2011-03-17 05:13:16 +0000

    Added refresh view action.

---
+++ trunk/plugin/src/maketargeter/actions/RefreshViewAction.java
@@ -0,0 +1,29 @@
+package maketargeter.actions;
+
+import maketargeter.MainView;
+import maketargeter.Plugin;
+
+import org.eclipse.jface.action.Action;
+
+/**
+ * @author Mike
+ */
+public class RefreshViewAction extends Action
+{
+	private final MainView m_view;
+	
+	public RefreshViewAction(MainView view)
+	{
+		super("Refresh view");
+		setImageDescriptor(Plugin.getImage("/icons/enabl/refresh.gif")); //$NON-NLS-1$
+		setDisabledImageDescriptor(Plugin.getImage("/icons/disabl/refresh.gif")); //$NON-NLS-1$
+		
+		m_view = view;
+	}
+
+	@Override
+	public void run()
+	{
+		m_view.onTargetsFileChanged();
+	}
+}
